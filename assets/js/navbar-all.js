13e0fb4fb416348f4675438449c3c44316601107
shubhamwork12532 <146088091+shubhamwork12532@users.noreply.github.com>
2025-04-19 10:26:23 +0530

    Common Nav Bar Script

--- assets/js/navbar-all.js
+++ assets/js/navbar-all.js
@@ -1,9 +1,14 @@
-var navbarContainer = document.getElementById("navbarContainerAll");
-var navbarRequest = new XMLHttpRequest();
-navbarRequest.open("GET", "navbar-all.html", true);
-navbarRequest.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    navbarContainer.innerHTML = this.responseText;
-  }
-};
-navbarRequest.send();
+// var navbarContainer = document.getElementById("navbarContainerAll");
+// var navbarRequest = new XMLHttpRequest();
+// navbarRequest.open("GET", "navbar-all.html", true);
+// navbarRequest.onreadystatechange = function () {
+//   if (this.readyState === 4 && this.status === 200) {
+//     navbarContainer.innerHTML = this.responseText;
+//   }
+// };
+// navbarRequest.send();
+fetch('navbar-all.html')
+.then(res => res.text())
+.then(data => {
+  document.getElementById('navbarContainerAll').innerHTML = data;
+});
